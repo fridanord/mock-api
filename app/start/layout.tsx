cd7013b4c53362fb605be4a1a9aed3c6922f6ed0
CJVIK <carl.viklund@hotmail.com>
2026-02-25 15:13:47 +0100

    Add Navbar component and integrate it into StartLayout for improved navigation

--- app/start/layout.tsx
+++ app/start/layout.tsx
@@ -1,21 +1,28 @@
 import AuthShell from "../(auth)/AuthShell";
 import LayoutAccordion from "../Components/LayoutAccordion";
+import Navbar from "../Components/Navbar";
 
 // START LAYOUTET som används i alla sidor under /start, innehåller en accordion med AuthShell och barnen som är själva sidorna
 export default function StartLayout({ children }: { children: React.ReactNode }) {
     return (
-        <LayoutAccordion
-            start={
-                <>
-                    <AuthShell />
-                    {children}
-                    {/* Här renderas själva innehållet i /start/login eller /start/register beroende på vilken flik som är aktiv, och AuthShell hanterar logiken för att visa rätt innehåll baserat på URL:en */}
-                </>
-            }
-            projects={<div className="p-8 text-white">Projects UI goes here</div>}
-            endpoints={<div className="p-8 text-white">Endpoints UI goes here</div>}
-            scenario={<div className="p-8 text-white">Scenario UI goes here</div>}
-        />
+        <main className="min-h-screen flex flex-col">
+            <Navbar />
+            <div className="flex-1 min-h-0">
+
+                <LayoutAccordion
+                    start={
+                        <>
+                            <AuthShell />
+                            {children}
+                            {/* Här renderas själva innehållet i /start/login eller /start/register beroende på vilken flik som är aktiv, och AuthShell hanterar logiken för att visa rätt innehåll baserat på URL:en */}
+                        </>
+                    }
+                    projects={<div className="p-8 text-white">Projects UI goes here</div>}
+                    endpoints={<div className="p-8 text-white">Endpoints UI goes here</div>}
+                    scenario={<div className="p-8 text-white">Scenario UI goes here</div>}
+                />
+            </div>
+        </main>
     );
 }
 // FÖRSLAG:
