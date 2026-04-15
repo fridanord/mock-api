e0a982904a367cdc882cd51b422027d060e4129f
Frida.Nord <Frida.Nord@medieinstitutet.se>
2026-04-15 14:18:59 +0200

    feat: add Scenario model and schema definition #57

---
+++ models/Scenario.ts
@@ -0,0 +1,36 @@
+import mongoose, { Schema, model, models } from "mongoose";
+
+const ScenarioSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Scenario name is required"],
+            trim: true
+        },
+        description: {
+            type: String
+        },
+        statusCode: {
+            type: Number,
+            required: true,
+            default: 200
+        },
+        responseBody: {
+            type: Schema.Types.Mixed,
+            required: true
+        },
+        isActive: {
+            type: Boolean,
+            default: false
+        },
+        endpointId: {
+            type: Schema.Types.ObjectId,
+            ref: "Endpoint",
+            required: true
+        },
+    },
+    { timestamps: true}
+);
+
+const Scenario = models.Scenario || model("Scenario", ScenarioSchema);
+export default Scenario;
